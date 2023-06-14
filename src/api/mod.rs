fdd010d1e49de4e26819fe5ee998e600c3e76cec
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-14 20:13:11 +0700

    Included get_cheapest_product in api

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,6 +3,10 @@ mod get_cheapest_product;
 pub async fn serve() -> tide::Result<()> {
     let mut app = tide::new();
 
+    app
+        .at("/get_cheapest_product")
+        .get(get_cheapest_product::get_cheapest_product);
+
     app.listen("127.0.0.1:8000").await?;
 
     Ok(())
