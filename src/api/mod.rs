09614eb64f432d456547c109ce9c577b05d24ff2
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-18 00:19:49 +0700

    Included cheapest_product.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,11 +1,11 @@
-mod get_cheapest_product;
+mod cheapest_product;
 
 pub async fn serve() -> tide::Result<()> {
     let mut app = tide::new();
 
-    app
-        .at("/get_cheapest_product")
-        .get(get_cheapest_product::get_cheapest_product);
+    app.at("/cheapest_product")
+        .get(cheapest_product::get)
+        .post(cheapest_product::post);
 
     app.listen("127.0.0.1:8000").await?;
 
