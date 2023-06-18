90f08254ff746dd552874fde0026fc4804cfbe3e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-18 23:03:13 +0700

    Removed the need for use

--- src/scraping/blibli/mod.rs
+++ src/scraping/blibli/mod.rs
@@ -1,11 +1,4 @@
-use scraper::html::Html;
-use crate::scraping::Product;
-use crate::scraping::ScrapeError;
-use crate::scraping::parse_document_from_url;
-use crate::scraping::parse_selector;
-use crate::scraping::get_first_text_from_parent_element_selector;
-
-pub fn parse_blibli_document(search_query: &str) -> Result<Html, ScrapeError> {
+pub fn parse_blibli_document(search_query: &str) -> Result<scraper::Html, super::ScrapeError> {
     let unformatted_url = "
         https://www.blibli.com/cari/%s
     ";
@@ -15,34 +8,36 @@ pub fn parse_blibli_document(search_query: &str) -> Result<Html, ScrapeError> {
         .replace("%s", search_query)
         .replace(" ", "");
 
-    parse_document_from_url(&url)
+    super::parse_document_from_url(&url)
 }
 
-pub fn get_blibli_products(search_query: &str) -> Result<Vec<Product>, ScrapeError> {
-    let mut products: Vec<Product> = Vec::new();
+pub fn get_blibli_products(search_query: &str) -> Result<Vec<super::Product>, super::ScrapeError> {
+    let mut products: Vec<super::Product> = Vec::new();
     let document = parse_blibli_document(search_query)?;
 
-    let product_selector = parse_selector(r#"div[class="product__item-container"]"#)?;
-    let product_name_selector = parse_selector(r#"div[class="product__title"]"#)?;
-    let product_price_selector = parse_selector(r#"strong[class="product__body__price__display"]"#)?;
-    let product_stars_selector = parse_selector(r#"span[class="product__body__rating__stars__rating"]"#)?;
+    let product_selector = super::parse_selector(r#"div[class="product__item-container"]"#)?;
+    let product_name_selector = super::parse_selector(r#"div[class="product__title"]"#)?;
+    let product_price_selector = super::parse_selector(r#"strong[class="product__body__price__display"]"#)?;
+    let product_stars_selector = super::parse_selector(r#"span[class="product__body__rating__stars__rating"]"#)?;
 
     for product_element in document.select(&product_selector) {
-        let product_name = get_first_text_from_parent_element_selector(
+        let product_name = super::get_first_text_from_parent_element_selector(
             &product_name_selector,
             product_element
-        )?;
+        )
+            .ok_or(super::ScrapeError::RetrieveElementNodeError)?;
 
-        let product_price_in_idr = get_first_text_from_parent_element_selector(
+        let product_price_in_idr = super::get_first_text_from_parent_element_selector(
             &product_price_selector,
             product_element
-        )?
+        )
+            .ok_or(super::ScrapeError::RetrieveElementNodeError)?
             .replace("Rp", "")
             .replace(".", "")
             .parse::<i32>()
-            .map_err(|_| ScrapeError::ParseElementNodeError)?;
+            .map_err(|_| super::ScrapeError::ParseElementNodeError)?;
 
-        let product_stars_text = get_first_text_from_parent_element_selector(
+        let product_stars_text = super::get_first_text_from_parent_element_selector(
             &product_stars_selector,
             product_element
         );
@@ -59,7 +54,7 @@ pub fn get_blibli_products(search_query: &str) -> Result<Vec<Product>, ScrapeErr
             Err(_) => {},
         }
 
-        products.push(Product {
+        products.push(super::Product {
             id: None,
             name: String::from(product_name),
             price_in_idr: product_price_in_idr,
